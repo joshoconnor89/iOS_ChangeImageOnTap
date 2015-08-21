072cceafd375649f9d0f68bada96c8000669af13
joshoconnor89 <joshoconnor89@gmail.com>
2015-08-21 16:15:17 -0700

    iOS Change Image On Tap
    iOS Change Image on tap project.  It is written in Swift.

---
+++ FlipImageTap.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CardFlip
+//
+//  Created by Ricky Yim on 30/07/2014.
+//  Copyright (c) 2014 Ricky Yim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var cardView: UIView!
+    var back: UIImageView!
+    var front: UIImageView!
+    
+    var showingBack = true
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        front = UIImageView(image: UIImage(named: "front.png"))
+        back = UIImageView(image: UIImage(named: "back.png"))
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: Selector("tapped"))
+        singleTap.numberOfTapsRequired = 1
+        let backImageWidth = back.image?.size.width
+        let backImageHeight = back.image?.size.height
+
+        let rect = CGRectMake(20, 20, backImageWidth!, backImageHeight!)
+        cardView = UIView(frame: rect)
+        cardView.addGestureRecognizer(singleTap)
+        cardView.userInteractionEnabled = true
+        cardView.addSubview(back)
+        
+        view.addSubview(cardView)
+    }
+
+
+    func tapped() {
+        if (showingBack) {
+            UIView.transitionFromView(back, toView: front, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+            showingBack = false
+        } else {
+            UIView.transitionFromView(front, toView: back, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+            showingBack = true
+        }
+
+    }
+
+}
+
